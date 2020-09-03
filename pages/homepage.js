fe1cf264d0303210eab2e59d34913e677da50e02
madapas <mada0193@stud.kea.dk>
2020-09-03 10:34:06 +0200

    page objects - structure

---
+++ pages/homepage.js
@@ -0,0 +1,13 @@
+class HomePage {
+    get pageHeader() {
+        return $('h1');
+    }
+    get subHeading() {
+        return $('div.banner-text-content > p.sub-text');
+    }
+    get platformLink() {
+        return $("//a[text()='Platform']");
+    }
+}
+
+module.exports = new HomePage();
---
+++ test/home.element.js
@@ -0,0 +1,13 @@
+const homePage = require('../pages/homepage');
+const { Browser } = require('selenium-webdriver');
+
+describe('Home page elements handle', () => {
+    
+    it('Verifies home page elements', () => {
+        browser.url('https://www.freshworks.com/');
+        console.log(homePage.pageHeader.getText());
+        console.log(homePage.subHeading.getText());
+        homePage.platformLink.click();
+        browser.pause(2000);
+    });
+});
